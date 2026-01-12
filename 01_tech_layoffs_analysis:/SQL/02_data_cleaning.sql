308d9cfc2bcc3f8afd6c55cfaeebf57c0f1e86b8
Poorna Venkat N <poornavenkatn@icloud.com>
2026-01-12 11:29:02 -0600

    Change author name in data cleaning script
    Updated author information in the SQL data cleaning script.

--- 01_tech_layoffs_analysis:/SQL/02_data_cleaning.sql
+++ 01_tech_layoffs_analysis:/SQL/02_data_cleaning.sql
@@ -1,9 +1,7 @@
 /*
 ==============================================================================
 PROJECT: Tech Industry Layoffs Analysis - Data Cleaning Pipeline
-AUTHOR: [Your Name]
-DATE CREATED: 2024
-LAST MODIFIED: December 2024
+AUTHOR: Poorna Venkat Neelakantam
 
 PURPOSE: 
 Comprehensive data cleaning and preparation pipeline for tech layoffs dataset.
